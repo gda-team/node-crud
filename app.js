c0e64bafb5d94dcd32ed3c75599fa306a364043b
Cedric karungu <ckarungu921@gmail.com>
2022-10-24 09:39:17 +0200

    feat:init express and config server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+
+dotenv.config();
+
+app.use((req, res, next) => res.send('hello world'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`app listening on Port ${PORT}`));
